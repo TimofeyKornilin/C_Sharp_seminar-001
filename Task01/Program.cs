02605695a4972259871cd7723af77057d68c322a
TimofeyKornilin <you@example.com>
2023-05-25 13:59:11 +0700

    Add Task O1 program SQUARE FOR NUMBER

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("");
+Console.WriteLine("*** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Square for {number} = {square}");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("*** END program ***");
+Console.WriteLine("");

91b8c7e665326861d5573a9605bd700197847be3
TimofeyKornilin <you@example.com>
2023-05-26 20:07:06 +0700

    Program write  last digit of three-digit number

---
+++ Task07/Program.cs
@@ -0,0 +1,23 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.Clear();
+Console.WriteLine("");
+Console.WriteLine("         *** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter your three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number % 10; 
+Console.WriteLine($"Last digit for your number is {lastDigit}");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("          *** END program ***");
+Console.WriteLine("");

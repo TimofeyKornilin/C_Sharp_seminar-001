d6638dd8181b1251f153e74a43548d4ef13e8725
TimofeyKornilin <you@example.com>
2023-05-26 19:49:40 +0700

    Program for N make -N .... 0 1 2 .... N

---
+++ Task05/Program.cs
@@ -0,0 +1,27 @@
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Clear();
+Console.WriteLine("");
+Console.WriteLine("         *** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = -number;
+Console.WriteLine(" ");
+
+while (number2 <= number)
+{
+    Console.Write($"{number2} ");
+    number2 = number2 + 1;
+}
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("          *** END program ***");
+Console.WriteLine("");

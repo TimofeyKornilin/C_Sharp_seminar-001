0a5251ffe1139bcdcd2cb2734f47885b9eea3b1c
TimofeyKornilin <you@example.com>
2023-05-26 20:53:07 +0700

    Program write last digit of positive and negative three-digit number

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -7,15 +7,20 @@
 
 
 Console.Clear();
-Console.WriteLine("");
 Console.WriteLine("         *** Start program ***");
 Console.WriteLine("");
 Console.WriteLine("");
 
 Console.Write("Enter your three-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int lastDigit = number % 10; 
-Console.WriteLine($"Last digit for your number is {lastDigit}");
+if (number < 0) number = -number;
+if (number > 100 && number < 1000)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Last digit for your number is {lastDigit}");
+}
+else Console.WriteLine("Your number is not three-digit number");
+
 
 Console.WriteLine("");
 Console.WriteLine("");

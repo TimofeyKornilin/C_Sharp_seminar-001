64c27a7821025e9a9ddcfe872d304cdbcd39579a
TimofeyKornilin <you@example.com>
2023-05-29 16:11:27 +0700

    Program write row of even number to any entered natural number

---
+++ Task08_HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("         *** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter natural number: ");
+double number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+
+if (number < 0) Console.WriteLine("Not correct entered number. Try again");
+else
+{
+    if (number < 2) Console.WriteLine("No have even numbers in this row");
+    else
+    {
+        double count = 1;
+        while (count <= number)
+            if (((count / 2) * 10) % 10 == 0)
+            {
+                Console.Write($"{count} ");
+                count++;
+            }
+            else count++;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("         *** END program ***");
+Console.WriteLine("");

9f7ecc8ebf0931ad32bfa505932c5c5d2a7af1b6
TimofeyKornilin <you@example.com>
2023-05-29 15:29:29 +0700

    Program write is the number even or not

---
+++ Task06_HomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6: Напишите программу, которая на вход
+// принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка)
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.WriteLine("         *** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter integer number: ");
+double number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+if (((number/2)*10) % 10 == 0) Console.WriteLine($"Yes, the number {number} is even number");
+else Console.WriteLine($"No, the number {number} is not even number");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("          *** END program ***");
+Console.WriteLine("");
